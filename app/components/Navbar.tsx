05cfdd9c2e89a5ff190af6728e212b0362b1ab43
andika <zoneandika@gmail.com>
2024-05-29 15:08:30 +0700

    create navbar 30%

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav className="w-full py-5 bg-blue-600 px-5">
+      <h1 className="text-[3rem] text-blue-900 font-aceng uppercase ">
+        pemira
+      </h1>
+    </nav>
+  );
+}
