776584927f2913b2fa218e5a1177e2a8745b4c99
andika <zoneandika@gmail.com>
2024-05-31 01:11:17 +0700

    membenarkan sikit

--- app/api/tes/route.ts
+++ app/api/tes/route.ts
@@ -2,12 +2,10 @@ import connectDB from "@/libs/connectDB";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    
-    try {
-        connectDB();
-        return NextResponse.json({ msg: "Hello world!" });
-    } catch (error) {
-        return NextResponse.json({ err: error });
-    }
-
+  try {
+    await connectDB();
+    return NextResponse.json({ msg: "aplikasi berjalan, database connected" });
+  } catch (error) {
+    return NextResponse.json({ err: error });
+  }
 }
--- libs/connectDB.ts
+++ libs/connectDB.ts
@@ -3,13 +3,14 @@ import mongoose from "mongoose";
 const connection: { isConnected?: number } = {};
 
 async function connectDB() {
-    if (connection.isConnected) {
-        return console.log("Gagal connect database!")
-    }
+  if (connection.isConnected) {
+    return console.log("Gagal connect database!");
+  }
 
-    const db = await mongoose.connect(process.env.MONGODB_URI!);
+  const db = await mongoose.connect(process.env.MONGODB_URI!);
+  console.log("berhasil koneksi db");
 
-    connection.isConnected = db.connections[0].readyState;
+  connection.isConnected = db.connections[0].readyState;
 }
 
 export default connectDB;
