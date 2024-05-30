062fe62c94e412c3fde8d03b0d96e0370078dc35
dimcoord <dimastio20062@gmail.com>
2024-05-30 12:37:17 +0700

    add patch method + fix error handling

--- app/api/siswa/route.ts
+++ app/api/siswa/route.ts
@@ -1,16 +1,42 @@
 import connectDB from "@/libs/connectDB";
 import Siswa from "@/models/Siswa";
 import { NextResponse } from "next/server";
+
 connectDB();
 
 export async function GET(req: any) {
-
+    
     try {
         const nis = await req.nextUrl.searchParams.get("nis");
         const dataSiswa = await Siswa.findOne({nis});
+        if (!nis) {
+            return NextResponse.json({msg: "NIS tidak boleh kosong!"}) 
+        } 
+        else if (dataSiswa == 'null') {
+            return NextResponse.json({msg: "NIS tidak ditemukan"}) 
+        }
         return NextResponse.json(dataSiswa);
-    } catch (err: any) {
-        return NextResponse.json({ error: err.message });
+    } catch (error) {
+        return NextResponse.json({ msg: "Gagal mengambil data siswa", err: error });
+    }
+
+}
+
+export async function PATCH(req: any) {
+    
+    try {
+        const nis = await req.nextUrl.searchParams.get("nis");
+        if (!nis) {
+            return NextResponse.json({msg: "NIS tidak boleh kosong!"})
+        }
+        const { pilihan } = await req.json();
+        if (pilihan !== undefined) {
+            const dataSiswa = await Siswa.findOneAndUpdate({nis}, { $set: { pilihan } });
+            return NextResponse.json({ msg: "Berhasil update data siswa" });
+        }
+        return NextResponse.json({ msg: "Pilihan tidak boleh kosong!" });
+    } catch (error) {
+        return NextResponse.json({ msg: "Gagal update data siswa", err: error });
     }
 
 }
