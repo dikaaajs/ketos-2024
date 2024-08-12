88dbb21a8ce3c22bb9b8b3963ca88f9ec6a20a59
andika <zoneandika@gmail.com>
2024-08-12 21:04:19 +0700

    nambahin anti save canche

--- app/api/hasil/route.ts
+++ app/api/hasil/route.ts
@@ -3,8 +3,8 @@ import Siswa from "@/models/Siswa";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  await connectDB();
   try {
+    await connectDB();
     const tmp = await Siswa.find();
     const countAll = tmp.length;
     console.log(countAll);
@@ -35,7 +35,12 @@ export async function GET() {
     result.ceketos[2].persen = (result.ceketos[2].count / countAll) * 100;
     result.golput.persen = (result.golput.count / countAll) * 100;
 
-    return NextResponse.json(result, { status: 200 });
+    const response = NextResponse.json(result, { status: 200 });
+    response.headers.set(
+      "Cache-Control",
+      "no-cache, no-store, must-revalidate"
+    );
+    return response;
   } catch (error) {
     return NextResponse.json(
       { msg: "Gagal mengambil data hasil pemilihan", err: error },
